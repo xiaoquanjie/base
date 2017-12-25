56ccd3b65ab05f89c84485d2b0f78bd0627664e6
xiaoquanjie <xiaoquanjie@ixianlai.com>
2017-12-25 17:14:42 +0800

    add tls

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "base/svector.hpp"
+#include "base/tls.hpp"
 
 using namespace std;
 
 int main() {
 
 	base::svector<int> vec;
+	base::tlsdata<int, 0>::data();
 
 	return 0;
 }
